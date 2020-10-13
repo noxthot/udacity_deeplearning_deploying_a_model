015ef8840533aab177ac2e3b941c060a4149dc1d
Gregor Ehrensperger <gregor.ehrensperger@gmx.com>
2020-10-13 22:15:33 +0200

    feat: implementing all the remaining functions and notebook execution outputs

--- serve/predict.py
+++ serve/predict.py
@@ -15,6 +15,7 @@ from model import LSTMClassifier
 
 from utils import review_to_words, convert_and_pad
 
+
 def model_fn(model_dir):
     """Load the PyTorch model from the `model_dir` directory."""
     print("Loading model.")
@@ -46,16 +47,21 @@ def model_fn(model_dir):
     print("Done loading model.")
     return model
 
+
 def input_fn(serialized_input_data, content_type):
     print('Deserializing the input data.')
+    
     if content_type == 'text/plain':
         data = serialized_input_data.decode('utf-8')
         return data
+    
     raise Exception('Requested unsupported ContentType in content_type: ' + content_type)
 
+    
 def output_fn(prediction_output, accept):
     print('Serializing the generated output.')
     return str(prediction_output)
+
 
 def predict_fn(input_data, model):
     print('Inferring sentiment of input data.')
@@ -69,9 +75,7 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -86,7 +90,7 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    modeloutput = model(data)
+    result = np.round(modeloutput.detach().numpy()).astype(np.int)
 
     return result
